1b00983d32b4f0493704a32bc77f8f744c8ab155
Sabrina <sbrnmchd@gmail.com>
2018-08-14 17:43:44 -0300

    Front Simples

---
+++ src/br/edu/insper/adicionarNota.java
@@ -0,0 +1,62 @@
+package br.edu.insper;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class adicionarNota
+ */
+@WebServlet("/adicionarNota")
+public class adicionarNota extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public adicionarNota() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void service(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+    	PrintWriter out = response.getWriter ();
+    	
+    	String nome = request.getParameter ("nome");
+    	String email = request.getParameter ("email");
+    	String idade = request.getParameter ("idade");
+    	
+    	out.println("<html>");
+    	out.println("<body>");
+    	out.println("Nome: " + nome);
+    	out.println("E-mail: " + email);
+    	out.println("Idade: " + idade);
+    	out.println("</body>");
+    	out.println("</html>");
+    	
+    	
+    }
+    
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
