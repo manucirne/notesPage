f05b242d887d227f735962fa321547573b382032
Sabrina <sbrnmchd@gmail.com>
2018-08-14 18:52:04 -0300

    começando a colocar campo de digitação

--- src/br/edu/insper/adicionarNota.java
+++ src/br/edu/insper/adicionarNota.java
@@ -30,15 +30,11 @@ public class adicionarNota extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
     	PrintWriter out = response.getWriter ();
     	
-    	String nome = request.getParameter ("nome");
-    	String email = request.getParameter ("email");
-    	String idade = request.getParameter ("idade");
+    	String nova_nota = request.getParameter ("nota");
     	
     	out.println("<html>");
     	out.println("<body>");
-    	out.println("Nome: " + nome);
-    	out.println("E-mail: " + email);
-    	out.println("Idade: " + idade);
+    	out.println(nova_nota);
     	out.println("</body>");
     	out.println("</html>");
     	
