20ac2350e7e58675150e69c6b9f93760782a3829
so-os-troxa <94878659+so-os-troxa@users.noreply.github.com>
2021-11-22 22:25:44 -0300

    removing unnecessary imports

--- src/br/edu/insper/ListaCor.java
+++ src/br/edu/insper/ListaCor.java
@@ -5,9 +5,6 @@ import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 @WebServlet("/listaCor")
